8816157be6fe0c83ed63ad877ab72a6050772769
Bernard Hamann <bernard@yebolocal.com>
2015-09-18 04:19:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var React = require('react');
+var Router = require('react-router');
+var beforeData = require('ne-data-before');
+
+var renderServer = function (server, routes, globals, pageAPIPath){
+
+    server.get('*', function (req, res) {
+
+        Router.run(routes, req.path, function (Root, state) {
+
+            var pathString = state.routes[1].path.substr(1);
+
+            function renderPage (data){
+                data.globals = globals;
+                state.data = data;
+                state.query = req.query;
+                console.log('Rendering ' + pathString + 'from Server - START');
+                var html = React.renderToStaticMarkup(React.createElement(Root, state));
+                var doctype = '<!DOCTYPE html>';
+                var fullHtml = doctype + html;
+                res.send(fullHtml);
+                console.log('Rendering ' + pathString + 'from Server - DONE');
+            }
+
+            beforeData.fetch(pageAPIPath, pathString)
+                .then(function(data) {
+                renderPage(data);
+            });
+
+        });
+    });
+};
+
+module.exports = renderServer;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ne-render-server (Node Engine Server Rendering)
+
+A tool to render pages Node Engine style on the server
