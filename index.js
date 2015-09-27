c7cbbe2d74f7acbd1025378571738a7d2ede9815
Bernard Hamann <bernard@yebolocal.com>
2015-09-27 08:17:45 +0200

    Api change

--- index.js
+++ index.js
@@ -12,33 +12,74 @@ var neRender = {
 
             Router.run(routes, req.path, function (Root, state) {
 
-                var pathString = state.routes[1].path.substr(1);
+                // Log pathString
+                console.log(state.routes[1].path.substr(1));
+                var neDataReqTimeoutTime = appmeta.config.neDataReqTimeoutTime || 6000;
+                var neDataReqTimeoutMessage = appmeta.config.neDataReqTimeoutMessage || "neDataReq Not Authorized";
 
-                console.log(pathString);
+                // Compile page meta
+                var meta = neData.meta(req, appmeta);
+                meta.query = req.query;
+                meta.params = state.params;
 
-                function renderPage (data){
+
+
+                // Render the page
+                function renderPage (data, fetchError){
+
+                    if (!fetchError) {
+                        stopTimeout();
+                    }
+                    if (fetchError) {
+                        data = {
+                            message: neDataReqTimeoutMessage
+                        };
+                    }
                     state.meta = meta;
                     state.data = data;
                     state.query = req.query;
+                    if (!fetchError) {
+                        stopTimeout();
+                        state.data = data;
+                    }
+                    if (req.user) {
+                        state.user = req.user;
+                    }
                     console.log('Rendering < ' + meta.title + ' > from Server - START');
                     var html = React.renderToStaticMarkup(React.createElement(Root, state));
                     var doctype = '<!DOCTYPE html>';
                     var fullHtml = doctype + html;
                     res.send(fullHtml);
                     console.log('Rendering ' + meta.title + 'from Server - DONE');
+
                 }
 
-                var meta = neData.meta(req, appmeta);
 
-                if (meta.nedBefore) {
+                // Handle errors with neData requests
+                var timeOut;
+                var startTimeout = function  (){
+                    timeOut = setTimeout(function(){
+                        var data = {};
+                        var fetchError = {};
+                        renderPage(data, fetchError);
+                    },neDataReqTimeoutTime);
+                };
+                var stopTimeout = function (){
+                    clearTimeout(timeOut);
+                };
+                startTimeout ();
 
-                    if (meta.nedCycle){
+
+
+                // Handle neData Requests
+                if (meta.neDataBefore) {
+
+                    if (meta.neDataCycle){
 
                         // this just uses the query params 'limit' and 'batch'
                         // to change the next and previous button url's and
                         // to get different sets of data for different values of limit and batch
                         // the cycle can be on before and after render data
-
                         neData.cycleBefore(meta)
                             .then(function(data) {
                                 renderPage(data);
